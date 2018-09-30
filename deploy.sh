43e329e03e370d34aa3758a7d9cdb1bfbf29374c
cocowh <596527851@qq.com>
2018-09-30 14:52:49 +0800

    update post

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,16 @@
+#!/bin/bash
 hexo generate
 cp -R public/* .deploy/cocowh.github.io
 cd .deploy/cocowh.github.io
+echo '--------master begin---------'
 git add -A
 git commit -m "update"
 git push origin master
+echo '--------master   end---------'
 cd ../..
+echo '--------backup begin---------'
 git add -A
 git commit -m 'update post'
 git push
+echo '--------backup   end---------'
 
